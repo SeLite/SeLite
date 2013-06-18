e87d1d9b60d5e0f06d21acd08b8de9a842ede168
peter.kehl <peter.kehl@gmail.com>
2013-06-18 17:58:59 +1000

    Fixed the license header - Renamed "Foobar" to actual project names

--- db-storage/src/chrome/content/extensions/db-storage.js
+++ db-storage/src/chrome/content/extensions/db-storage.js
@@ -1,18 +1,18 @@
 /*  Copyright 2011, 2012 Peter Kehl
     This file is part of SeLite DbStorage.
 
-    SeLite DbStorage is free software: you can redistribute it and/or modify
+    SeLite DB Storage is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    SeLite DB Storage is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with SeLite DB Storage.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 "use strict";
