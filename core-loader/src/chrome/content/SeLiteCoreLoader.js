414fd08507ea83a5f2a23fe8465cb827764f5c14
peter-kehl <peter.kehl@gmail.com>
2013-09-11 07:08:34 +1000

    Using SeLiteCoreLoader

--- core-loader/src/chrome/content/SeLiteCoreLoader.js
+++ core-loader/src/chrome/content/SeLiteCoreLoader.js
@@ -1,3 +1,19 @@
+/*  Copyright 2013 Peter Kehl
+    This file is part of SeLite Core Loader.
+
+    SeLite Core Loader is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SeLite Core Loader is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SeLite Core Loader.  If not, see <http://www.gnu.org/licenses/>.
+*/
 "use strict";
 
 function SeLiteCoreLoader() {}
