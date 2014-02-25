1c968b6500b2c2434e45060f9797975bf4d6f3de
peter.kehl <peter.kehl@gmail.com>
2014-02-26 09:55:09 +1100

    Renamed sel-blocks-global.js to sel-blocks.js, so that the name matches the one in original SelBlocks extension. This is for future merges with SelBlocks

--- selblocks-global/src/chrome/content/SeLiteExtensionSequencerManifest.js
+++ selblocks-global/src/chrome/content/SeLiteExtensionSequencerManifest.js
@@ -1,6 +1,6 @@
 SeLiteExtensionSequencer.registerPlugin( {
     pluginId: 'selblocks-global@selite.googlecode.com',
-    coreUrl: 'chrome://selite-selblocks-global/content/extensions/selblocks-global.js',
+    coreUrl: 'chrome://selite-selblocks-global/content/extensions/sel-blocks.js',
     xmlUrl: 'chrome://selite-selblocks-global/content/reference.xml',
     requisitePlugins: {
         'testcase-debug-context@selite.googlecode.com': 'SeLite TestCase Debug Context',
