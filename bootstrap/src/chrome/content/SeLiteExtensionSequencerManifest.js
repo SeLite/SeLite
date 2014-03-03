12d2978eb3748a2c454f55de4bb224ef0f86ffe1
peter.kehl <peter.kehl@gmail.com>
2014-03-04 10:48:59 +1100

    Bootstrap dependant on SelBlocksGlobal; XPI v. 0.76

--- bootstrap/src/chrome/content/SeLiteExtensionSequencerManifest.js
+++ bootstrap/src/chrome/content/SeLiteExtensionSequencerManifest.js
@@ -2,7 +2,10 @@ SeLiteExtensionSequencer.registerPlugin( {
     pluginId: 'bootstrap@selite.googlecode.com',
     coreUrl: 'chrome://selite-bootstrap/content/extensions/se_bootstrap.js',
     ideUrl: "chrome://selite-bootstrap/content/logic/SeBootstrap.js",
-    requisitePlugins: { 'testcase-debug-context@selite.googlecode.com': 'SeLite TestCase Debug Context' },
+    requisitePlugins: {
+        'testcase-debug-context@selite.googlecode.com': 'SeLite TestCase Debug Context',
+        'selblocks-global@selite.googlecode.com': 'SeLite SelBlocksGlobal'
+    },
     callBack: function(api) {
         // Based on PluginFramework.prototype.addPluginProvidedStringPreference() from https://addons.mozilla.org/en-US/firefox/addon/file-logging-selenium-ide/
         api.preferences.DEFAULT_OPTIONS["se_bootstrap_scriptFileName"] = api.preferences.getString("se_bootstrap_scriptFileName", '');
