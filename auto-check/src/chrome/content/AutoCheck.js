74d86849ae6e5882fd9ddb421b0bb67570087cf3
peter.kehl <peter.kehl@gmail.com>
2014-03-18 12:30:29 +1100

    AutoCheck

--- auto-check/src/chrome/content/AutoCheck.js
+++ auto-check/src/chrome/content/AutoCheck.js
@@ -24,7 +24,7 @@ var SeLiteAutoCheck= {};
      * When debugging failedXYZ() methods, beware that running a single verification that fails (by double-clicking) doesn't log any message about the failure. It only highlights the command (in the editor matrix) in red/pink. Only when you run a test case/suite then any failed verifications log their messages in the log (in red).
      *  @param {object} required Object serving as an array of entries matching any required contents. In default implementation an entry can be any Selenese locator.
      *  @param {object) refused Object serving as an array of entries matching any refused contents. In default implementation an entry can be any Selenese locator.
-     *  @param {object) ignored Object serving as an array of entries matching any ignored notices/warnings/errors. In default implementation an entry can be any Selenese locator.
+     *  @param {object) ignored Object serving as an array of entries matching any ignored notices/warnings/errors. The detail of what these entries can be is up to the implementation of failedNotIngored(document) in the actual subclass.
      *  @param {boolean} assert Whether to run the checks as assertions; otherwise they are run as validation only. Optional, false by default.
      * */
     SeLiteAutoCheck.Detector= function Detector( required, refused, ignored, assert ) {
@@ -70,7 +70,7 @@ var SeLiteAutoCheck= {};
     /** Auto Check for PHP (optionally with XDebug).
      * @param {object} required Object of locators.
      * @param {object} refused Object of locators.
-     * @param {object} ignored Object of XPath expressions. They must be relative within Xdebug container (if used) - use '.' to refer to that container. They must not start with '//' or 'xpath='. (This doesn't allow location methods other than XPath. See implementation of Selenium IDE's eval_locator()). It must not include the header-like 'Notice', 'Warning', 'Error'.
+     * @param {object} ignored Object of logical XPath conditions - expressions that fit into [] part of XPath. They match warnings/notices/errors that are not to be reported. They can refer to the matched text node as '.'. They should not contain relative references to parent/sibling nodes. They can match an error message (a part after 'Notice', 'Warning', 'Error') or the file path of the leaf file where the error comes from. If you follow this, the matching works whether you use PHP with Xdebug or without it.
      *  @param {boolean} assert See the same parameter of SeLiteAutoCheck.Detector().
     */
     SeLiteAutoCheck.DetectorPHP= function DetectorPHP( required, refused, ignored, assert ) {
@@ -85,24 +85,25 @@ var SeLiteAutoCheck= {};
         if( !fromXdebug ) {
             // Following matches one node per error - the description/message
             errorElements= eval_xpath( "//b[ .='Notice' or .='Warning' or .='Error' ]/following-sibling::node()[1][ starts-with(., ': ') ]", document );
-            // I could match both the description and file path by one regex, but it may be less flexible:
+            // I could match both the description and file path by one regex, but matching and handling results would be more complicated:
             // "//b[ .='Notice' or .='Warning' or .='Error' ][ following-sibling::node()[1][starts-with(., ': ') and following-sibling::node()[2][contains(., 'on line')] ] ]/following-sibling::node()[ 1 ]"
         }
-        errorElementLoop: //@TODO do we get more entries for the same error?!
+        errorElementLoop:
         for( var i=0; i<errorElements.length; i++ ) { //@TODO for..of..
             var errorElement= errorElements[i];
-            var errorFileElements= undefined; // element containing the file path where the errors wa (if displayed and if !fromDebug)
+            var errorFileElements= undefined; // element containing the file path where the errorr was reported (if displayed and if !fromDebug)
             // If fromXdebug, then I don't match the file location separately - it's already within errorElement
             if( !fromXdebug ) {
                 //errorFileElements= eval_xpath( "./following-sibling::node()[1]", document, {contextNode: errorElement} );
                 errorFileElements= eval_xpath( "/following-sibling::b[1]", document, {contextNode: errorElement} );
             }
             for( var key in this.ignored ) {
-                if( eval_xpath( this.ignored[key], document, {contextNode: errorElement} ).length!==0 ) {
+                var ignoredXPath= '/self::node()[' +this.ignored[key]+ ']';
+                if( eval_xpath( ignoredXPath, document, {contextNode: errorElement} ).length!==0 ) {
                     continue errorElementLoop;
                 }
                 if( errorFileElements && errorFileElements.length!==0 ) {
-                    if( eval_xpath( this.ignored[key], document, {contextNode: errorFileElements[0]} ).length!==0 ) {
+                    if( eval_xpath( ignoredXPath, document, {contextNode: errorFileElements[0]} ).length!==0 ) {
                         continue errorElementLoop;
                     }                        
                 }
