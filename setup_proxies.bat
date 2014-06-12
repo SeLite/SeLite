c15f4fba0c234be92f72bd16a580e88388e5514f
peter.kehl <peter.kehl@gmail.com>
2014-06-12 14:45:20 +1000

    Setup proxies for Win

---
+++ setup_proxies.bat
@@ -0,0 +1,73 @@
+@echo off
+for /f "tokens=*" %%a in ('dir  %APPDATA%\Mozilla\Firefox\Profiles\*.default /b') do set p=%APPDATA%\Mozilla\Firefox\Profiles\%%a
+if defined p (
+    set e=%p%\extensions
+    REM If you have not got any extensions in Firefox profile yet, there is no 'extensions' folder. So create it.
+    if not exist %e% (
+      mkdir %e%
+    )
+    
+    REM Now set up the actual extensions, unless they are already installed from XPI    
+    REM Do not use: echo %CD%.>target-file. Use: cd >target-file. For some reason %CD% doesn't get updated after I change directory.
+    if not exist %e%\auto-check@selite.googlecode.com.xpi (
+      cd auto-check\src
+      cd > %e%\auto-check@selite.googlecode.com
+      cd ..\..
+    )
+
+    if not exist %e%\bootstrap@selite.googlecode.com.xpi (
+      cd bootstrap\src
+      cd > %e%\bootstrap@selite.googlecode.com
+      cd ..\..
+    )
+
+    if not exist %e%\extension-sequencer@selite.googlecode.com.xpi (
+      cd extension-sequencer\src
+      cd > %e%\extension-sequencer@selite.googlecode.com
+      cd ..\..
+    )
+
+    if not exist %e%\settings@selite.googlecode.com.xpi (
+      cd settings\src
+      cd > %e%\settings@selite.googlecode.com
+      cd ..\..
+    )
+
+    if not exist %e%\sqlite-connection-manager@selite.googlecode.com.xpi (
+      cd sqlite-connection-manager\src
+      cd > %e%\sqlite-connection-manager@selite.googlecode.com
+      cd ..\..
+    )
+
+    if not exist %e%\testcase-debug-context@selite.googlecode.com.xpi (
+      cd testcase-debug-context\src
+      cd > %e%\testcase-debug-context@selite.googlecode.com
+      cd ..\..
+    )
+
+    REM if not exist %e%\selblocks-global@selite.googlecode.com.xpi (
+    REM  cd selblocks-global\src
+    REM  cd > %e%\selblocks-global@selite.googlecode.com
+    REM  cd ..\..
+    REM )
+    
+    if not exist %e%\misc@selite.googlecode.com.xpi (
+      cd misc\src
+      cd > %e%\misc@selite.googlecode.com
+      cd ..\..
+    )
+
+    if not exist %e%\db-objects@selite.googlecode.com.xpi (
+      cd db-objects\src
+      cd > %e%\db-objects@selite.googlecode.com
+      cd ..\..
+    )
+
+    if not exist %e%\commands@selite.googlecode.com.xpi (
+      cd commands\src
+      cd > %e%\commands@selite.googlecode.com
+      cd ..\..
+    )
+) else (
+   echo Could not find a default Firefox profile
+)
