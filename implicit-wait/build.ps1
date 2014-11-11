58369e187698a11561eec35645600f603a5a403f
peter.kehl <peter.kehl@gmail.com>
2014-11-11 15:03:45 +1100

    Import of Implicit Wait from SVN revision 39

---
+++ implicit-wait/build.ps1
@@ -0,0 +1,135 @@
+﻿#########################################################################################################################################
+#    "C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe" -noexit -File "%1" 
+#    "C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe" -command "Set-ExecutionPolicy RemoteSigned"
+#########################################################################################################################################
+
+mode con cols=120 lines=60
+
+###################### Save allPath ###################################
+
+$Project_name = "SeleniumImplicitWait"
+$Setup_dir = [System.IO.Directory]::GetCurrentDirectory() + "\"
+$Project_dir = (get-item $Setup_dir).parent.fullname + "\"
+$VersionFile_path = $Sources_dir + "install.rdf"
+
+set-alias cmd-7zip "C:\Program Files\7-Zip\7z.exe"
+set-alias cmd-firefox "C:\Program Files\Mozilla Firefox\firefox.exe"
+
+function getVersion($version){
+    $new_version =""
+    while ($new_version -eq ""){
+        $input = read-host "   Digit to increment [x.y.z] or version [0.0.0] or skip [s] ? "
+        if ($input -match "s|z|y|x") {
+            $version_digit = $version -split "\."
+            switch ($input){
+                "s" { $new_version = $version }
+                "z" { $new_version = $version_digit[0] + "." + $version_digit[1] + "." + [string]([int]$version_digit[2]+1) }
+                "y" { $new_version = $version_digit[0] + "." + [string]([int]$version_digit[1]+1) + ".0" }
+                "x" { $new_version = [string]([int]$version_digit[0]+1) + ".0.0" }
+            }
+        }else{
+            if ($input -match "\d+\.\d+\.\d+") { $new_version = $input }
+        }
+    }
+    return $new_version
+}
+
+function getSha1($filepath){
+    [String]$sha1 =""
+    $sha1o = New-Object System.Security.Cryptography.SHA1CryptoServiceProvider
+    $file = [System.IO.File]::Open($filepath, "open", "read")
+    $sha1o.ComputeHash($file)| %{ $sha1 += $_.ToString("x2") }
+    $file.Close()
+    return $sha1
+}
+
+write-host ""
+write-host " --------------------------------- $Project_name -------------------------------------------"
+
+write-host ""
+write-host "        > Manual       ** Automatic"
+write-host ""
+
+#Check folders and files
+(gi Env:PATH).value.split(";")| ForEach {if(!(test-path $_)){write-host("  Error ENV:PATH : Folder " + $_ + """ not found" ) -ForegroundColor Red;}}
+Get-Variable -name *_dir | ForEach {if(!(test-path $_.Value)){write-host("  Error : Folder " + $_.Name + "=""" + $_.Value + """ not found") -ForegroundColor Red;}}
+Get-Variable -name *_path | ForEach {if(!(test-path $_.Value)){write-host("  Error : File " + $_.Name + "=""" + $_.Value + """ not found") -ForegroundColor Red;}}
+Get-Alias -name cmd-* | ForEach {if(!(test-path $_.Definition)){write-host("  Error : Program " + $_.Name + "=""" + $_.Definition + """ not found") -ForegroundColor Red;}}
+
+#Get the version from update.rdf and subversion URL
+$VersionFile_txt = (([regex]::matches((get-content $VersionFile_path), "<em:version>(\d+\.\d+\.\d+)<"))[0]).Groups[1].Value;
+
+#Get last compilation date
+$LastCompil_date = if ((test-path($VersionFile_path)) -eq 1){(get-item( $VersionFile_path )).LastWriteTime}
+
+#Write information on the screen
+write-host " __________________________________________________________________________________________________"
+write-host ""
+write-host "  Project name    : "  $Project_name
+write-host "  Directory       : "  $Project_dir
+write-host "  Current Version : "  $VersionFile_txt
+write-host "  Last creation   : "  $LastCompil_date
+write-host " __________________________________________________________________________________________________"
+write-host ""
+
+write-host " 0-Edit the version number :"
+	[String]$f_get_version = getVersion($VersionFile_txt)
+	write-host "   New version : " $f_get_version
+	write-host "   ** Update the version in update.rdf ..."
+		$file=$VersionFile_path
+		#$oldstring="<em:version>(\d+\.\d+\.\d+)<"
+		#$newstring="<em:version$f_get_version<"
+		#(get-content $file ) | % {$_ -replace $oldstring, $newstring } | Set-Content -path $file
+        [XML]$xmlDoc = [XML](gc $VersionFile_path)
+        $xmlDoc.RDF.Description.version = [string]$f_get_version
+        $xmldoc.Save($file)
+	
+    write-host ""
+
+write-host " 1-Create package :"
+	$ZipInclude_list= @("chrome\*.*","chrome.manifest","install.rdf")
+	$ZipExclude_list= @()
+	$OutputXpi = $Project_name + "-" + $f_get_version + ".xpi"
+    write-host "   ** Create the package $OutputXpi ..."
+    	if(test-path($OutputXpi)){ Remove-Item $OutputXpi; }
+    	cmd-7zip a $OutputXpi  -tzip -r ($ZipInclude_list|ForEach{$_}) ($ZipExclude_list|ForEach{"-x!"+$_}) |  out-Null
+        if($LASTEXITCODE -eq 1) { write-host("  Package creation failed ! ") -ForegroundColor red; break; }
+        
+    write-host ""
+
+write-host " 2-Edit update.rdf :"
+	write-host "   ** Calculate the sha1 of $OutputXpi..."
+    [String]$updateHash = "sha1:" + ( getSha1($OutputXpi) )
+    [String]$updateLink = "http://selenium-implicit-wait.googlecode.com/files/" + $OutputXpi
+    [XML]$xmlDoc = [XML](gc "update.rdf")
+	$firstchild = $xmlDoc.RDF.Description.updates.Seq.li | Select-Object -first 1
+    if($firstchild.Description.version -ne $f_get_version){
+		write-host "   ** Insert a new version description..."
+        $newchild = $firstchild.Clone()
+        $newchild.Description.version=$f_get_version
+        $firstchild = $xmlDoc.RDF.Description.updates.Seq.InsertBefore($newchild,$firstchild)
+    }else{
+		write-host "   ** Update the version description..."
+	}
+    $firstchild.Description.targetApplication.Description.updateLink = $updateLink
+    $firstchild.Description.targetApplication.Description.updateHash = $updateHash
+    $xmldoc.save("update.rdf")
+    
+    write-host ""
+ 
+write-host " 2-Package user-extensions :"
+	write-host "   ** Create zip file for user-extensions.js ..."
+	$OutputExtZip = $Project_name + "-user-extensions-" + $f_get_version + ".zip"
+	$ZipInclude_list= @("user-extensions.js")
+	$ZipExclude_list= @()
+    cmd-7zip a $OutputExtZip  -tzip -r ($ZipInclude_list|ForEach{$_}) ($ZipExclude_list|ForEach{"-x!"+$_}) |  out-Null
+    if($LASTEXITCODE -eq 1) { write-host("  Package creation for user-extension failed ! ") -ForegroundColor red; break; }
+    
+    write-host ""
+ 	
+write-host " 2-Install in firefox :"
+	write-host "   ** Launching Firefox for installation..."
+	cmd-firefox $OutputXpi
+ 
+$input = read-host "   Press enter to quit "
+exit
---
+++ implicit-wait/chrome/content/implicit-wait-ide.js
@@ -0,0 +1,193 @@
+/*
+ * Copyright 2012 Florent Breheret
+ * http://code.google.com/p/selenium-implicit-wait/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+(function(){
+
+/**
+ * Class: Adds the implicit wait feature to SeleniumIDE.
+ * @param {Object} editor
+ */
+function ImplicitWait(editor){
+    this.editor = editor;
+    
+    setTimeout(function(){      //waits all the sub-scripts are loaded to wrap selDebugger.init 
+        wrap(editor.selDebugger, 'init', this, this.wrap_selDebugger_init);
+    }.bind(this), 0);
+}
+ImplicitWait.prototype = {
+    
+    DEFAULT_TIMEOUT: 5000,
+    
+    wait_forced: false,
+    wait_timeout: 0,
+    postcondition_timeout: 0,
+    postcondition_func: null,
+    postcondition_run: null,
+    
+    /** Callback for the click on the  hourglass button*/
+    toggleButton: function(button) {
+        if((this.wait_forced = (button.checked ^= true)))
+            this.wait_timeout = this.DEFAULT_TIMEOUT;
+        else
+            this.wait_timeout = 0;
+    },
+    
+    /** Call from the setImplicitWait command*/
+    setImplicitWait: function(timeout){
+        this.wait_timeout = +timeout || 0;
+    },
+        
+    /** Call from the setImplicitWaitCondition command*/
+    setImplicitWaitCondition: function(timeout, condition_js){
+        if((this.postcondition_timeout = +timeout || 0)){
+            this.postcondition_func = new Function('return ' + condition_js);
+            this.postcondition_func.__string__ = condition_js;
+        }else{
+            this.postcondition_func = null;
+        }
+        this.postcondition_run = null;
+    },
+    
+    /** Overrides Debugger.init: function() in debugger.js line 23 */
+    wrap_selDebugger_init: function(base, fn, args/*[]*/){
+        fn.apply(base, args);   //calls the original method
+        base.runner.MozillaBrowserBot.prototype.findElement = BrowserBot_findElement;
+        wrap(base.runner.IDETestLoop.prototype, 'resume', this, this.wrap_IDETestLoop_resume);
+        this.wait_timeout = (this.wait_forced && this.DEFAULT_TIMEOUT) || 0;
+        this.postcondition_timeout = 0;
+        this.postcondition_func = this.postcondition_run = null;
+    },
+    
+    /** Overrides TestLoop.prototype.resume: function() in selenium-executionloop.js line 71 */
+    wrap_IDETestLoop_resume: function(base, fn, args/*[]*/){
+        var selDebugger = this.editor.selDebugger,
+            runner = selDebugger.runner,
+            selenium = runner.selenium,
+            browserbot = selenium.browserbot,
+            LOG = runner.LOG;
+        
+        LOG.debug("currentTest.resume() - actually execute modified");
+        browserbot.runScheduledPollers();
+        
+        var command = base.currentCommand;
+        LOG.info("Executing: |" + command.command + " | " + command.target + " | " + command.value + " |");
+
+        var handler = base.commandFactory.getCommandHandler(command.command);
+        if (handler === null)
+            throw new SeleniumError("Unknown command: '" + command.command + "'");
+            
+        command.target = selenium.preprocessParameter(command.target);
+        command.value = selenium.preprocessParameter(command.value);
+        LOG.debug("Command found, going to execute " + command.command);
+        
+        runner.updateStats(command.command);
+        
+        var locator_endtime = this.wait_timeout && new Date().getTime() + this.wait_timeout;
+        var self = this;
+        (function loopFindElement(){
+            try{
+                base.result = handler.execute(selenium, command);
+                base.waitForCondition = base.result.terminationCondition;
+                (function loopCommandCondition(){    //handles the andWait condition in replacement of continueTestWhenConditionIsTrue
+                    try{
+                        browserbot.runScheduledPollers();
+                        if(base.waitForCondition && !base.waitForCondition())
+                            return selDebugger.state !== 2/*PAUSE_REQUESTED*/ && window.setTimeout(loopCommandCondition, 15);
+                        base.waitForCondition = null;
+                        var postcondition_endtime = self.postcondition_run && new Date().getTime() + self.postcondition_timeout;
+                        self.postcondition_run = self.postcondition_func;
+                        (function loopPostCondition(){    //handles the customized postcondition
+                            if(postcondition_endtime){
+                                try{
+                                    if(new Date().getTime() > postcondition_endtime)
+                                        base.result = {failed: true, failureMessage: 'Timed out on postcondition ' + self.postcondition_func.__string__};
+                                    else if(!self.postcondition_func.call(selenium))
+                                        return selDebugger.state !== 2/*PAUSE_REQUESTED*/ && window.setTimeout(loopPostCondition, 15);
+                                }catch(e){
+                                     base.result = {failed: true, failureMessage: 'Exception on postcondition ' + self.postcondition_func.__string__ + '  Exception:' + extractExceptionMessage(e)};
+                                }
+                            }
+                            base.commandComplete(base.result);
+                            base.continueTest();
+                        })();
+                    }catch(e){
+                        base.result = {failed: true, failureMessage: extractExceptionMessage(e)};
+                        base.commandComplete(base.result);
+                        base.continueTest();
+                    }
+                })();
+            }catch(e){
+                if(e.isElementNotFoundError && locator_endtime && new Date().getTime() < locator_endtime)
+                    return selDebugger.state !== 2/*PAUSE_REQUESTED*/ && window.setTimeout(loopFindElement, 20);
+                if(base._handleCommandError(e))
+                    base.continueTest();
+                else
+                    base.testComplete();
+            }
+        })();
+    }
+};
+
+
+/** 
+ * Overriding for BrowserBot.prototype.findElement: function(locator, win) in selenium-browserbot.js line 1524
+ * @param {String} locator
+ * @param {Object} win
+ */
+var BrowserBot_findElement = function (locator, win){
+    var element = this.findElementOrNull(locator, win);
+    if(element === null)
+        throw new ElementNotFountError(locator);
+    return window.core.firefox.unwrap(element);
+};
+
+
+/**
+ * Class: Error specific to findElement
+ * @param {String} locator
+ */
+function ElementNotFountError(locator) {
+    this.locator = locator;
+}
+ElementNotFountError.prototype = {
+    isSeleniumError: true,
+    isElementNotFoundError: true,
+    get message(){ return 'Element ' + this.locator + ' not found'; },
+    toString: function(){ return this.message; }
+};
+
+
+/**
+ * Wraps a method call to a function on a specified context. Skips the wrapping if already done.
+ * @param {Object} obj Object containing the function to intercept
+ * @param {String} key Name of the function to intercept
+ * @param {Object} context Object on which the intercepting function func will be applied
+ * @param {Function} func  Function intercepting obj[method] : function(context, function, arguments)
+ */
+function wrap(obj, key, context, func){
+    var fn = obj[key], w;
+    if(!(w=fn.__wrap__) || w.src !== fn || w.tgt !== func ){
+        (obj[key] = function(){
+            return func.call(context, this, fn, arguments);
+        }).__wrap__ = {src:fn, tgt:func};
+    }
+}
+
+
+//Instantiates the plug-in
+this.editor.implicitwait = new ImplicitWait(this.editor);
+
+})();
