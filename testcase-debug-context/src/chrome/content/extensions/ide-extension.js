e8ac54200caa907016c60791a6199c2a637ce9ce
peter.kehl <peter.kehl@gmail.com>
2014-11-12 14:16:59 +1100

    In-progress

--- testcase-debug-context/src/chrome/content/extensions/ide-extension.js
+++ testcase-debug-context/src/chrome/content/extensions/ide-extension.js
@@ -22,7 +22,6 @@ editor.testLoopResume= function testLoopResume() {
     var selenium = runner.selenium;
     var browserbot = selenium.browserbot;
     var LOG = runner.LOG;
-    var base= editor.selDebugger;
     
     // From TestLoop's resume() in selenium-executionloop.js:
     LOG.debug("currentTest.resume() - actually execute modified");
@@ -31,81 +30,106 @@ editor.testLoopResume= function testLoopResume() {
     runner.Selenium.seLiteBeforeCurrentCommand.call();
 
     // Following is from TestLoop's _executeCurrentCommand() in selenium-runner.js
-    var command = base.currentCommand;
+    var command = this.currentCommand;
     LOG.info("Executing: |" + command.command + " | " + command.target + " | " + command.value + " |");
 
-    // I've moved 'var handler' to testLoopResumeExecuteAndHandleErrors()
+    var command = this.currentCommand;
+    var handler = this.commandFactory.getCommandHandler(command.command);
+    if (handler === null) {//@TODO error handling as per resume()?
+        throw new SeleniumError("Unknown command: '" + command.command + "'");
+    }
+    
     command.target = selenium.preprocessParameter(command.target);
     command.value = selenium.preprocessParameter(command.value);
     LOG.debug("Command found, going to execute " + command.command);
 
     runner.updateStats(command.command);
-    // end of code based on _executeCurrentCommand() - except for its three lines, which are handled in testLoopResumeExecuteAndHandleErrors() below
+    // end of code based on _executeCurrentCommand() - except for a few lines, which are handled in testLoopResumeExecuteAndHandleErrors() below
+    
+    editor.testLoopResumeExecuteAndHandleErrors.call( this, command, handler );
 };
-editor.testLoopResumeExecuteAndHandleErrors= function testLoopResumeExecuteAndHandleErrors() {
+/**
+    It's based on:
+    - the rest of _executeCurrentCommand() that wasn't processed by testLoopResume() itself above, and
+    - a call to continueTestWhenConditionIsTrue() and
+    - error handling from TestLoop's resume() in selenium-executionloop.js:
+ * */
+editor.testLoopResumeExecuteAndHandleErrors= function testLoopResumeExecuteAndHandleErrors( command, handler ) {
     var selDebugger = editor.selDebugger;
     var runner = selDebugger.runner;
     var selenium = runner.selenium;
     var browserbot = selenium.browserbot;
-    var LOG = runner.LOG;
-    var base= selDebugger;
     
-    // Following replaces:
-    // - three lines from _executeCurrentCommand() and
-    // - a call to this.continueTestWhenConditionIsTrue() and
-    // - error handling from TestLoop's resume() in selenium-executionloop.js:
-    var handler = base.commandFactory.getCommandHandler(command.command);
-    if (handler === null) {
-        throw new SeleniumError("Unknown command: '" + command.command + "'");
-    }
-    var locator_endtime = this.wait_timeout && new Date().getTime() + this.wait_timeout;
-    var self = this;
+    //@TODO editor.implicitwait:
+    var locator_endtime = editor.implicitwait.wait_timeout && new Date().getTime() + editor.implicitwait.wait_timeout;
+    var self = editor.implicitwait;
     var loopFindElement= function loopFindElement() {
         try{
-            base.result = handler.execute(selenium, command); // from _executeCurrentCommand()
-            base.waitForCondition = base.result.terminationCondition; // from _executeCurrentCommand()
+            //@TODO change most or all: selDebugger -> this
+            selDebugger.result = handler.execute(selenium, command); // from _executeCurrentCommand()
+            selDebugger.waitForCondition = selDebugger.result.terminationCondition; // from _executeCurrentCommand()
             var loopCommandCondition= function loopCommandCondition() {    //handles the andWait condition in replacement of continueTestWhenConditionIsTrue
                 try{
                     browserbot.runScheduledPollers();
-                    if(base.waitForCondition && !base.waitForCondition())
+                    if( selDebugger.waitForCondition && !selDebugger.waitForCondition() ) {
                         return selDebugger.state !== 2/*PAUSE_REQUESTED*/ && window.setTimeout(loopCommandCondition, 15);
-                    base.waitForCondition = null;
+                    }
+                    selDebugger.waitForCondition = null;
                     var postcondition_endtime = self.postcondition_run && new Date().getTime() + self.postcondition_timeout;
                     self.postcondition_run = self.postcondition_func;
                     var loopPostCondition= function loopPostCondition() {    //handles the customized postcondition
                         if(postcondition_endtime){
                             try{
-                                if(new Date().getTime() > postcondition_endtime)
-                                    base.result = {failed: true, failureMessage: 'Timed out on postcondition ' + self.postcondition_func.__string__};
-                                else if(!self.postcondition_func.call(selenium))
+                                if( new Date().getTime() > postcondition_endtime ) {
+                                    selDebugger.result = {failed: true, failureMessage: 'Timed out on postcondition ' + self.postcondition_func.__string__};
+                                }
+                                else if( !self.postcondition_func.call(selenium) ) {
                                     return selDebugger.state !== 2/*PAUSE_REQUESTED*/ && window.setTimeout(loopPostCondition, 15);
+                                }
                             }catch(e){
-                                 base.result = {failed: true, failureMessage: 'Exception on postcondition ' + self.postcondition_func.__string__ + '  Exception:' + extractExceptionMessage(e)};
+                                 selDebugger.result = {failed: true, failureMessage: 'Exception on postcondition ' + self.postcondition_func.__string__ + '  Exception:' + extractExceptionMessage(e)};
                             }
                         }
-                        runner.Selenium.seLiteAfterCurrentCommand.call( base );
-                        base.commandComplete(base.result);
-                        base.continueTest();
+                        runner.Selenium.seLiteAfterCurrentCommand.call( selDebugger );
+                        selDebugger.commandComplete(selDebugger.result);
+                        selDebugger.continueTest();
                     };
                     loopPostCondition();
                 }catch(e){
-                    base.result = {failed: true, failureMessage: extractExceptionMessage(e)};
-                    base.commandComplete(base.result);
-                    base.continueTest();
+                    selDebugger.result = {failed: true, failureMessage: extractExceptionMessage(e)};
+                    selDebugger.commandComplete(selDebugger.result);
+                    selDebugger.continueTest();
                 }
             };
             loopCommandCondition();
         } catch(e){
             if(e.isElementNotFoundError && locator_endtime && new Date().getTime() < locator_endtime)
                 return selDebugger.state !== 2/*PAUSE_REQUESTED*/ && window.setTimeout(loopFindElement, 20);
-            if(base._handleCommandError(e))
-                base.continueTest();
+            if(selDebugger._handleCommandError(e))
+                selDebugger.continueTest();
             else
-                base.testComplete();
+                selDebugger.testComplete();
         }
     };
     loopFindElement();
 };
+
+editor.testLoopResumeExecuteAndHandleErrors= function testLoopResumeExecuteAndHandleErrors( command, handler ) {
+    var selenium = editor.selDebugger.runner.selenium;
+    try{
+        this.result = handler.execute(selenium, command); // from _executeCurrentCommand()
+        this.waitForCondition = this.result.terminationCondition; // from _executeCurrentCommand()
+        this.continueTestWhenConditionIsTrue(); // from resume()
+    }
+    catch( e ) {
+        if (!this._handleCommandError(e)) {
+            this.testComplete();
+        } else {
+            this.continueTest();
+        }
+    }
+};
+
 setTimeout( //waits until all the sub-scripts are loaded to overload selDebugger.init
     function() {
         
