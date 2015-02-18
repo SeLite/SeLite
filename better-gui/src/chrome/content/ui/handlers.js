4e0ffb77224d9c56f7ee7a6b44fde8299f024493
peter.kehl <peter.kehl@gmail.com>
2015-02-18 11:08:34 +1100

    Better gui

--- better-gui/src/chrome/content/ui/handlers.js
+++ better-gui/src/chrome/content/ui/handlers.js
@@ -7,33 +7,33 @@ function onTreeClick( event ) {
     // event.target is treechildren; event.currentTarget is tree; event.relatedTarget is null
     // @TODO if event.clientY is too close to the bottom of the tree, then return. Otherwise the following selected a cell in a neighbour row!
     var tree= event.currentTarget;
-    var row= { value: -1 }; // value will be 0-based row index, within the set of *visible* rows only (it skips the collapsed rows)
-    var column= { value: null }; // value is instance of TreeColumn. See https://developer.mozilla.org/en-US/docs/XPCOM_Interface_Reference/nsITreeColumn                
+    var rowObject= { value: -1 }; // value will be 0-based row index, within the set of *visible* rows only (it skips the collapsed rows)
+    var columnObject= { value: null }; // value is instance of TreeColumn. See https://developer.mozilla.org/en-US/docs/XPCOM_Interface_Reference/nsITreeColumn                
     var treeBoxObject= tree.boxObject;
     treeBoxObject.QueryInterface(Components.interfaces.nsITreeBoxObject);
-    treeBoxObject.getCellAt(event.clientX, event.clientY, row, column, {}/*unused, but needed*/ );
+    treeBoxObject.getCellAt(event.clientX, event.clientY, rowObject, columnObject, {}/*unused, but needed*/ );
+    var column= columnObject.value;
     
     // The clicked row has already been selected as the current command/comment.
     // For commands, only allow edit-in-place for 'target' and 'value' columns. That allows us to still execute the command by double-clicking at the command name itself.
     // For comments, only allow edit-in-place for 'command' column, and do it no matter what column was clicked.
-    if( editor.treeView.currentCommand.type==='command' && (column.value===tree.columns[1] || column.value===tree.columns[2])
+    if( editor.treeView.currentCommand.type==='command' && (column===tree.columns[1] || column===tree.columns[2])
     ||  editor.treeView.currentCommand.type==='comment'
     ) {
         if( editor.treeView.currentCommand.type==='comment' ) {
-            column.value= tree.columns[0];
+            column= tree.columns[0];
             //@TODO temporarily disable overflow?!
         }
-        tree.startEditing( row.value, column.value );
+        tree.startEditing( rowObject.value, column );
 
         // The above call to startEditing() calls setTimeout(), which puts a callback function in the execution queue. That callback function focuses and selects the tree.inputField. The following puts another code in the queue, which simulates a click at tree.inputField, so that the user can start typing where she clicked. See also chrome://global/content/bindings/tree.xml#tree -> startEditing
         window.setTimeout( function() {
-            if( editor.treeView.currentCommand.type==='comment' ) {
-                //@TODO If the user clicked at a long comment (that overflew to target/value column), we focus at the last visible character in the editable area.
-                // I.e. if event.X is past the tree.inputField (i.e. past 'command' column), then use tree.inputField's right coordinate
-            }
-            var range= document.caretPositionFromPoint(event.clientX, event.clientY);
-            tree.inputField.setSelectionRange( range.offset, range.offset );
-            //treeBoxObject.invalidateScrollbar(); This doesn't help
+            // If the user clicked at a long comment (that overflew to target/value column), we put the caret after the last character in the editable area.
+            // I tried to put it after the last *visible* character, but I couldn't find a way. E.g. document.caretPositionFromPoint( window.left+tree.inputField.left+tree.inputField.width-20, event.clientY) returned null.
+            var caretCharacterIndex= editor.treeView.currentCommand.type==='command' || columnObject.value===tree.columns[0]
+                ? document.caretPositionFromPoint( event.clientX, event.clientY).offset
+                : tree.inputField.value.length;
+            tree.inputField.setSelectionRange( caretCharacterIndex, caretCharacterIndex );
         }, 0 );
    }
 }
