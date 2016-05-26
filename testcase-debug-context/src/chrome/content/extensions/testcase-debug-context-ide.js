8b7fefb24b8a203e42465a17ee5f37b8d6c0e9e1
peter-kehl <peter.kehl@gmail.com>
2016-05-26 15:09:35 +1000

    For catching timeouts and exeptions from continuation test conditions.

--- testcase-debug-context/src/chrome/content/extensions/testcase-debug-context-ide.js
+++ testcase-debug-context/src/chrome/content/extensions/testcase-debug-context-ide.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Peter Kehl
+ * Copyright 2014, 2016 Peter Kehl
  * Based on code from Selenium IDE, which is Copyright 2011 Software Freedom Conservancy.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
