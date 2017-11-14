03aa16f27eb5dc583e73f0df174ef9f892c643eb
peter-kehl <peter.kehl@gmail.com>
2017-11-14 15:58:56 +1100

    Migration notice

--- db-objects/src/chrome/content/Db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/** This is a placeholder-like object. All files from SeLite DB Objects add their functionality to this object.
- *  @namespace
- * */
-var SeLiteData= {};
-
-var EXPORTED_SYMBOLS=['SeLiteData'];
