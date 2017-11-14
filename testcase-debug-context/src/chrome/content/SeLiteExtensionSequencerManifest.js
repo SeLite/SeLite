b71eb958618bc2af4c6e415262fed21dda30da24
peter-kehl <peter.kehl@gmail.com>
2017-11-14 16:29:00 +1100

    Migration notice

--- testcase-debug-context/src/chrome/content/SeLiteExtensionSequencerManifest.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-SeLiteExtensionSequencer.registerPlugin( {
-    name: 'SeLite TestCase Debug Context',
-    id: 'testcase-debug-context@selite.googlecode.com',
-    coreURL: 'chrome://selite-testcase-debug-context/content/extensions/testcase-debug-context-core.js',
-    ideURL: 'chrome://selite-testcase-debug-context/content/extensions/testcase-debug-context-ide.js',
-    infoURL: 'https://addons.mozilla.org/en-US/firefox/addon/selite-testcase-debug-conte/'
-} );
