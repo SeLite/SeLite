a905abfd6fb3fe428bd259a8e861892612595b08
peter-kehl <peter.kehl@gmail.com>
2017-11-14 12:29:42 +1100

    Migration notice in SQLite Connection Manager.

--- sqlite-connection-manager/src/chrome/content/SeLiteExtensionSequencerManifest.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-SeLiteExtensionSequencer.registerPlugin( {
-    name: 'SeLiet SQLite Connection Manager',
-    id: 'sqlite-connection-manager@selite.googlecode.com',
-    infoURL: 'https://addons.mozilla.org/en-US/firefox/addon/selite-testcase-debug-conte/'
-} );
